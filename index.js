33c23b93ff48da1f294db7622274b06fb21b9fb6
GustavoCruz <gustavo0201cruz@gmail.com>
2023-01-06 20:51:53 -0400

    adicionado cobre

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function verificaItem() {
    const objItem = {
 
     value: 0,
+    valueCopper: 0,
     rarity: 0,
     quant: 0
 
@@ -18,14 +19,17 @@ function verificaItem() {
    if (type.value == '1' || type.value == '4') {
 
     objItem.value = 100000
+    objItem.valueCopper = 800000
 
    }else if(type.value =='2'){
 
     objItem.value = 800000
+    objItem.valueCopper = 800000
 
    }else{
 
     objItem.value = 50000
+    objItem.valueCopper = 400000
 
    }
 
@@ -54,33 +58,41 @@ function calcularItem () {
         const itemData = verificaItem()
         let parametroDS = 0
         let parametroPO = 0
+        let parametroCP = 0
         let dsTotal = 0
         let poTotal = 0
+        let cpTotal = 0
 
 
         if (itemData.rarity == 1) {
             
             parametroDS = 5000
             parametroPO = 25
+            parametroCP = 20000
 
         }else{
 
             parametroDS = 25000
             parametroPO = 125
+            parametroCP = 100000
 
         }
 
         dsTotal = (itemData.quant*(300 + 100 + 100)*parametroDS)
         poTotal = (itemData.quant*(300 + 100 + 100)*parametroPO)
+        cpTotal = (itemData.quant*(300 + 100 + 100)*parametroCP)
 
         let açoDS = material1.value*parametroDS
         let açoPO = material1.value*parametroPO
+        let açoCP = material1.value*parametroCP
 
         let mat2DS = material2.value*parametroDS
         let mat2PO = material2.value*parametroPO
+        let mat2CP = material2.value*parametroCP
 
         let mat3DS = material3.value*parametroDS
         let mat3PO = material3.value*parametroPO
+        let mat3CP = material3.value*parametroCP
 
         console.log(açoDS);
         console.log(mat2DS);
@@ -88,10 +100,11 @@ function calcularItem () {
 
         dsTotal = dsTotal + (itemData.value * itemData.quant) - açoDS - mat2DS - mat3DS
         poTotal = poTotal - açoPO - mat2PO - mat3PO
+        cpTotal = cpTotal + (itemData.valueCopper * itemData.quant) - açoCP - mat2PO - mat3CP
 
         console.log(itemData.value);
 
-        result.innerHTML = `Você precisa de ${poTotal} pó brilhante, ${dsTotal} de Dark Stell.`
+        result.innerHTML = `Você precisa de ${poTotal} pó brilhante, ${cpTotal} de cobre e ${dsTotal} de Dark Stell.`
 
     })
 
